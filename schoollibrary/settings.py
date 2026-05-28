889da1087c654c37f7e64a0b6f410ce480dc8b19
Basil Okoth <162017549+BasilOkoth@users.noreply.github.com>
2026-05-28 18:49:59 +0300

    Change LOGIN_URL to '/smart-login/'

--- schoollibrary/settings.py
+++ schoollibrary/settings.py
@@ -194,7 +194,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-LOGIN_URL = "/login/"
+LOGIN_URL = "/smart-login/"
 LOGIN_REDIRECT_URL = "/app/"
 
 # =========================
