5553c38b3f1a5d58fbb755b8bfa6ace83975c7fb
Basil Okoth <162017549+BasilOkoth@users.noreply.github.com>
2026-05-29 23:14:43 +0300

    Add allowed host for schoollibrary deployment

--- schoollibrary/settings.py
+++ schoollibrary/settings.py
@@ -14,6 +14,7 @@ DEBUG = config("DEBUG", default=True, cast=bool)
 ALLOWED_HOSTS = [
     "localhost", "127.0.0.1",
     ".localhost",
+    "schoollibrary-1.onrender.com",
     ".onrender.com", ".render.com",
     "shulehub.org", "www.shulehub.org", ".shulehub.org",
     "miyuga.localhost", "oluti.localhost", "daraja.localhost",
