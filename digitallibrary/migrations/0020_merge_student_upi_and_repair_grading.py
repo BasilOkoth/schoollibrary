9f6c98672ea5bfda663048e789e1564a680aba66
Basil Okoth <162017549+BasilOkoth@users.noreply.github.com>
2026-06-18 21:51:03 +0000

    Remove unresolved conflict markers from views

---
+++ digitallibrary/migrations/0020_merge_student_upi_and_repair_grading.py
@@ -0,0 +1,13 @@
+# Generated manually to merge parallel digitallibrary migration branches.
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("digitallibrary", "0013_alter_student_upi_number"),
+        ("digitallibrary", "0019_repair_grading_tables"),
+    ]
+
+    operations = []
