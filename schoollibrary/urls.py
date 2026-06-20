28d45bde62cf3c6d1de5b9c9db117c804b3014fa
Basil Okoth <162017549+BasilOkoth@users.noreply.github.com>
2026-06-20 10:44:35 +0300

    Update urls.py

--- schoollibrary/urls.py
+++ schoollibrary/urls.py
@@ -1,6 +1,6 @@
 from functools import wraps
 import logging
-
+from tenants import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
