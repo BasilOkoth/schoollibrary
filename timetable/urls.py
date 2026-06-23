a668638de721a42c07b235d2e9a53cdcb21d7051
Basil Okoth <162017549+BasilOkoth@users.noreply.github.com>
2026-06-23 22:56:03 +0300

    Update urls.py

--- timetable/urls.py
+++ timetable/urls.py
@@ -7,5 +7,13 @@ urlpatterns = [
     path("", views.timetable_dashboard, name="dashboard"),
     path("class/", views.class_timetable_view, name="class_timetable"),
     path("teacher/", views.teacher_timetable_view, name="teacher_timetable"),
+
+    path("manage/", views.timetable_manage, name="manage"),
+    path("template/add/", views.timetable_template_create, name="template_add"),
+    path("day/add/", views.timetable_day_create, name="day_add"),
+    path("period/add/", views.timetable_period_create, name="period_add"),
+    path("room/add/", views.timetable_room_create, name="room_add"),
+    path("entry/add/", views.timetable_entry_create, name="entry_add"),
+
     path("tv/current-lessons/", views.timetable_tv_current_lessons, name="tv_current_lessons"),
 ]
