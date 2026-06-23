4de904b42f720272942a2a98bacb9827c7ba5e0a
Basil Okoth <162017549+BasilOkoth@users.noreply.github.com>
2026-06-23 21:52:39 +0300

    Update urls.py

--- timetable/urls.py
+++ timetable/urls.py
@@ -5,5 +5,7 @@ app_name = "timetable"
 
 urlpatterns = [
     path("", views.timetable_dashboard, name="dashboard"),
+    path("class/", views.class_timetable_view, name="class_timetable"),
+    path("teacher/", views.teacher_timetable_view, name="teacher_timetable"),
     path("tv/current-lessons/", views.timetable_tv_current_lessons, name="tv_current_lessons"),
 ]
