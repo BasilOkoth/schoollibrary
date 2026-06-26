740fcfd9198fd7526f4e80566728e22e45925b1b
Basil Okoth <162017549+BasilOkoth@users.noreply.github.com>
2026-06-26 12:34:52 +0300

    Update forms.py

--- digitallibrary/forms.py
+++ digitallibrary/forms.py
@@ -236,7 +236,7 @@ class ResourceForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields["subject"].queryset = Subject.objects.all().order_by("name")
+        self.fields["subject"].queryset = Subject.objects.all().order_by("result_code", "name")
         self.fields["subject"].empty_label = "--- Select Subject ---"
         self.fields["subject"].required = False
         self.fields["subject"].label = "Subject"
